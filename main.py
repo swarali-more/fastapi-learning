51ee9ed9cc24a740294b6a2cac6e0a241bea3a91
Swarali <swaralimore3@gmail.com>
2026-03-25 17:17:21 +0530

    Initial FastAPI project setup

---
+++ main.py
@@ -0,0 +1,21 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def home():
+    return {"message":"Hello Swarali!"}
+
+
+@app.get("/about")
+def about():
+    return {"message":"I am Swarali, Python Backend Developer!"}
+
+@app.get("/user/{name}")
+def get_user(name:str):
+    return {"message":f"Hello {name},Welcome to mt API !"}
+
+    
+@app.get("/multiply/{x}/{y}")
+def multi(x:int,y:int):
+    return {"result": x * y}
